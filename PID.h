25aad663589582756eea77cef91ed85ddcf5ae02
Mikhail <shinsei75@gmail.com>
2020-12-05 13:35:00 +0300

    Add files via upload

---
+++ PID.h
@@ -0,0 +1,10 @@
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+int PID(float input, float setpoint, float kp, float ki, float kd, float dt, int minOut, int maxOut) {
+  float err = setpoint - input;
+  static float integral = 0, prevErr = 0;
+  integral = constrain(integral + (float)err * dt * ki, minOut, maxOut);
+  float D = (err - prevErr) / dt;
+  prevErr = err;
+  return constrain(err * kp + integral + D * kd, minOut, maxOut);	
+}
